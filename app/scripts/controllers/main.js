aa30c279955c5f327ef63d24ff95a15263b8d5f8
Katsiaryna Utlik <Katsiaryna_Utlik@epam.com>
2014-12-09 13:29:36 +0300

    HTML layout

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,4 @@
  */
 angular.module('feedbackFormApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
